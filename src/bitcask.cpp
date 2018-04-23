da677ced5c56a9181e46c49fbacfbf2cc3f78de2
fall0death <503703217@qq.com>
2018-04-23 17:31:23 +0800

    git出bug

--- src/bitcask.cpp
+++ src/bitcask.cpp
@@ -1,49 +1,53 @@
+
+
+
 #include "bitcask.h"
 
 
-namespace bitcask {
+namespace bitcask{
 
-    Bitcask::Bitcask() : active_hint_id(0), active_data_id(0) {
+Bitcask::Bitcask(){
+    active_data_id = 0;
+    active_hint_id = 0;
+}
+
+Bitcask::~Bitcask(){
+    this->close();
+}
+
+bool close(){
+    if(){
+        
     }
+}
 
-    Bitcask::~Bitcask() {
-        this->close();
-    }
+void init(){
+    
+}
 
-    bool close() {
-        if (active_data_file.idopen()) {
+bool _insert(){
+    
+}//增操作
+bool _update(){
+    
+}//改操作
+bool _delete(){
+    
+}//删操作
+bool _select(){
+    
+}//查操作
+bool _merge(){
+    
+}//整合数据
 
-        }
-    }
+bool write_file(){
+    
+}
+bool read_file(){
+    
+}
 
-    void init() {
-
-    }
-
-    bool _insert() {
-
-
-    }//增操作
-    bool _update() {
-
-    }//改操作
-    bool _delete() {
-
-    }//删操作
-    bool _select() {
-
-    }//查操作
-    bool _merge() {
-
-    }//整合数据
-
-    bool write_file() {
-
-    }
-
-    bool read_file() {
-
-    }
 
 
 }
